f6913930964e37e1257c1366ca811d0b64627df2
Karen Fang <karenfang@Karens-MacBook-Pro.local>
2021-10-01 23:22:44 -0700

    finish pdf form filer - add text, checkbox, combo filer

--- forms/filer.py
+++ forms/filer.py
@@ -31,8 +31,10 @@ https://stackoverflow.com/questions/17742042/how-to-fill-pdf-form-in-python
 ANNOT_KEY = '/Annots'           # key for all annotations within a page
 ANNOT_FIELD_KEY = '/T'          # Name of field. i.e. given ID of field
 ANNOT_FORM_type = '/FT'         # Form type (e.g. text/button)
+ANNOT_FORM_flag = '/Ff'         # Form flag
 ANNOT_FORM_button = '/Btn'      # ID for buttons, i.e. a checkbox
 ANNOT_FORM_text = '/Tx'         # ID for textbox
+ANNOT_FORM_choice = '/Ch'       # ID for choice box
 SUBTYPE_KEY = '/Subtype'
 WIDGET_SUBTYPE_KEY = '/Widget'
 PARENT_KEY = '/Parent'
@@ -45,12 +47,29 @@ barcode_list = ['form1[0].#subform[0].PDF417BarCode1[0]',
                 'form1[0].#subform[5].PDF417BarCode1[5]',
                 'form1[0].#subform[6].PDF417BarCode1[6]']
 
+# update text section
 def text_form(annotation, value):
     annotation.update(pdfrw.PdfDict(V='{}'.format(value)))
     annotation.update(pdfrw.PdfDict(AP=''))
     #pdfstr = pdfrw.objects.pdfstring.PdfString.encode(value)
     #annotation.update(pdfrw.PdfDict(V=pdfstr, AS=pdfstr))
-    
+
+# update choice box section
+def combobox(annotation, value):
+    export = None
+    for each in annotation['/Opt']:
+        if each[1].to_unicode() == value:
+            export = each[0].to_unicode()
+    if export is None: 
+        raise KeyError(f"Export Value: {value} Not Found")
+        
+    #pdfstr = pdfrw.objects.pdfstring.PdfString.encode(export)
+    #annotation.update(pdfrw.PdfDict(V=pdfstr, AS=pdfstr))
+    annotation.update(pdfrw.PdfDict(V='{}'.format(export), AS='{}'.format(export)))
+    annotation.update(pdfrw.PdfDict(AP=''))
+
+
+# update checkbox section
 def checkbox(annotation, key_value, export=None):
     if export:
         export = '/' + export
@@ -65,6 +84,36 @@ def checkbox(annotation, key_value, export=None):
         del annotation['/V']
         del annotation['/AS']
 
+# update list box section
+def listbox(annotation, values):
+    pdfstrs=[]
+    for value in values:
+        export=None
+        for each in annotation['/Opt']:
+            if each[1].to_unicode()==value:
+                export = each[0].to_unicode()
+        if export is None:
+            raise KeyError(f"Export Value: {value} Not Found")
+    
+    pdfstrs.append(pdfrw.objects.pdfstring.PdfString.encode(export))
+    annotation.update(pdfrw.PdfDict(V=pdfstrs, AS=pdfstrs))
+
+# update radio button section
+def radio_button(annotation, value):
+    for each in annotation['/Kids']:
+        # determine the export value of each kid
+        keys = each['/AP']['/N'].keys()
+        keys.remove('/Off')
+        export = keys[0]
+
+        if f'/{value}' == export:
+            val_str = pdfrw.objects.pdfname.BasePdfName(f'/{value}')
+        else:
+            val_str = pdfrw.objects.pdfname.BasePdfName(f'/Off')
+        each.update(pdfrw.PdfDict(AS=val_str))
+
+    annotation.update(pdfrw.PdfDict(V=pdfrw.objects.pdfname.BasePdfName(f'/{value}')))
+        
 def fill_pdf(input_pdf_path, output_pdf_path, data_dict):
     template_pdf = pdfrw.PdfReader(input_pdf_path)
     
@@ -74,6 +123,8 @@ def fill_pdf(input_pdf_path, output_pdf_path, data_dict):
             continue
 
         for annotation in annotations:
+            ft = annotation[ANNOT_FORM_type]
+            ff = annotation[ANNOT_FORM_flag]
             if annotation[SUBTYPE_KEY]==WIDGET_SUBTYPE_KEY:
                 if not annotation[ANNOT_FIELD_KEY]:
                     annotation=annotation[PARENT_KEY]
@@ -85,36 +136,23 @@ def fill_pdf(input_pdf_path, output_pdf_path, data_dict):
                     if key in data_dict.keys():
                         print("Key: ", key)
                         print("data_dict_value: ", data_dict[key])
-                        if annotation[ANNOT_FORM_type] == ANNOT_FORM_button: # fill checkbox
-                            print(annotation['/AP']['/N'].keys())
-                            checkbox(annotation, data_dict[key], export=None)
+                        if ft == ANNOT_FORM_button: # fill checkbox
+                            if ff and int(ff) & 1 << 15:
+                                radio_button(annotation, data_dict[key])
+                            else:
+                                print(annotation['/AP']['/N'].keys())
+                                checkbox(annotation, data_dict[key], export=None)
                     
                         if annotation[ANNOT_FORM_type] == ANNOT_FORM_text: # fill text
                             text_form(annotation, data_dict[key])
-    
-    
-    '''
-    for page in template_pdf.pages:
-        annotations = page[ANNOT_KEY]
-        for annotation in annotations:
-            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-                if annotation[ANNOT_FIELD_KEY]:
-                    key = annotation[ANNOT_FIELD_KEY][1:-1]
-                    if key in barcode_list:
-                        continue
                         
-                    if key in data_dict.keys():
-                        if type(data_dict[key]) == bool:
-                            if data_dict[key] == True:
-                                #annotation.update(pdfrw.PdfDict(AS=pdfrw.PdfName('On')))
-                                #annotation.update(pdfrw.PdfDict(V=pdfrw.PdfName('On'), AS = pdfrw.PdfName('Yes')))
-                                annotation.update(pdfrw.PdfDict(V=pdfrw.objects.pdfname.BasePdfName('/I')))
-                        else:
-                            annotation.update(
-                                pdfrw.PdfDict(V='{}'.format(data_dict[key]))
-                            )
-                            annotation.update(pdfrw.PdfDict(AP=''))
-    '''
+                        if ft == ANNOT_FORM_choice: # fill combo choice
+                            if ff and int(ff) & 1 << 17:
+                                combobox(annotation, data_dict[key])
+                            else: 
+                                listbox(annotation, data_dict[key])
+                                
+                        
     template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
     pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
 
@@ -161,7 +199,7 @@ if __name__ == '__main__':
         "form1[0].#subform[0].P1_Line2b_Date[0]": "01/31/2022",
         "form1[0].#subform[0].P1_Line1_2a_Checkbox[0]": True, 
         "form1[0].#subform[0].P1_Line1_2a_Checkbox[1]": True,
-        "form1[0].#subform[0].P1_Line4a_Name[0]": "",
+        "form1[0].#subform[0].P1_Line4a_Name[0]": "test name",
         "form1[0].#subform[0].P1_Line4b_Street[0]": "39967 Waxwing Drive",
         "form1[0].#subform[0].P1_Line4c_Number[0]": 123,
         "form1[0].#subform[0].P1_Line4c_Unit[0]": True,
@@ -177,7 +215,7 @@ if __name__ == '__main__':
         "form1[0].#subform[0].P1_Line5f_Date[0]": "test",
         "form1[0].#subform[0].P1_Line5e_Checkbox[0]": True,
         "form1[0].#subform[0].P1_Line4f_ZipCode[0]": '94560',
-        "form1[0].#subform[0].P1_Line4e_State[0]": 'CA',
+        "form1[0].#subform[0].P1_Line4e_State[0]": 'FL',
         "form1[0].#subform[0].P1_Line4d_City[0]": 'Newark',
         "form1[0].#subform[0].PDF417BarCode1[0]":""
     }
